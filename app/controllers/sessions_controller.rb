c3bea06b9551ae7c9f8d9066a45c2d81817785e3
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-28 21:55:14 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    WebMock.disable_net_connect!(allow_localhost: true)
     response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
       req.params[:client_id] = ENV['GITHUB_CLIENT']
       req.params[:client_secret] = ENV['GITHUB_SECRET']
