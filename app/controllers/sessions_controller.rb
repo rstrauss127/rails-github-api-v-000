2a45d5a436e63c324f70eb5eb553e2ef75f0cf37
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-28 22:03:35 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
       raise params.inspect
-      req.body = { 'client_id': ENV['GITHUB_CLIENT'], 'client_secret': ENV['GITHUB_SECRET'], 'code': params[:code] }
+      req.body = { 'client_id': ENV['GITHUB_CLIENT'], 'client_secret': ENV['GITHUB_SECRET'], 'code': params["code"] }
       req.headers['Accept'] = 'application/json'
     end
     body = JSON.parse(response.body)
