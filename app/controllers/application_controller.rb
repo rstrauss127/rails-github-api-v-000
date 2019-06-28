7d5dcf68f4f191fb34582e824209a00fe641a64b
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-28 22:30:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     client_secret = ENV['GITHUB_CLIENT']
     redirect_uri ="http://localhost:3000/auth"
     github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
-    raise github_url.inspect
+
     redirect_to github_url unless logged_in?
   end
 
