d6dfecd37ba55bb3b90acf5675a1b43a08f40b13
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-28 20:37:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    client_id = ENV['GITHUB_CLIENT']
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
   end
 
   def logged_in?
