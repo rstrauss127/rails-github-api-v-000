b7bfe693c6108dbfdc7b6e0f3ed6c5acb7ad3d06
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-28 21:38:22 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,10 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
   def create
-    client_id = ENV['GITHUB_CLIENT']
     response = Faraday.post("http://github.com/login/oauth/access_token") do |req|
       req.params[:client_id] = ENV['GITHUB_CLIENT']
+      req.params[:client_secret] = ENV['GITHUB_SECRET']
+      req.params[:code] = params[:code]
     end
     puts response
   end
